9a9d6b020f7113036628c2ae4c4abbdfd8dfab7d
samarthsgithub <123881350+samarthsgithub@users.noreply.github.com>
2024-09-17 05:07:07 +0530

    Update signin.js

--- frontend/src/pages/signin.js
+++ frontend/src/pages/signin.js
@@ -3,8 +3,9 @@ import React, { useState,useContext,useEffect} from 'react';
 import {useNavigate} from "react-router-dom";
 import axios from 'axios'; // Import axios
 import { UserContext } from "../context/UserContext";
-const API_URL = process.env.REACT_APP_API_URL;
 import '../styles/Auth.css'; // Importing the CSS file for styling
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 const Signin = () => {
   const { user, setUser } = useContext(UserContext);
